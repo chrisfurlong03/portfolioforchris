366ceaadfef00ce1cf8c034ff3875b7566cb3d51
chrisfurlong03 <chrisfurlong03@gmail.com>
2022-01-11 10:58:06 +1300

    started adding “load more” functionality

--- website.js
+++ website.js
@@ -17,20 +17,33 @@ var json = {
       link: "01-07-2022.html",
       img: "https://i.imgur.com/L8s3VFt.jpg",
       color: "blue-200"
+    },
+    {
+      title: "Tsunami Research Project / My IB EE",
+      link: "01-11-2022.html",
+      img: "https://i.imgur.com/y5505xZ.jpg",
+      color: "sky-200"
+    },
+    {
+      title: "Kristin School Sustainability Website",
+      link: "01-12-2022.html",
+      img: "https://i.imgur.com/yFADY7t.png",
+      color: "teal-100"
     }
   ]
 };
 
 var doPosts = document.getElementsByClassName("do-blog")[0];
 var doItems = json.doItems;
-for (var i = 0; i < doItems.length; i++) {
+
+//for (var i = 0; i < doItems.length; i++) {
+for (var i = 0; i < 3; i++) {
   var div = document.createElement("div");
   div.classList.add(
     "bg-" + doItems[i].color,
     "w-64",
     "h-24",
     "ml-2",
-    "md:ml-5",
     "mt-2",
     "flex"
   );
@@ -61,3 +74,17 @@ for (var i = 0; i < doItems.length; i++) {
   div.appendChild(img);
   div.appendChild(p);
 }
+var btn = document.createElement("div");
+btn.classList.add(
+  "bg-gray-100",
+  "hover:bg-gray-200",
+  "ml-2",
+  "mt-2",
+  "max-h-[3.25rem]"
+);
+
+btn.innerHTML =
+  "<a class='not-italic px-8 py-3 text-gray-700 w-full flex items-center justify-center hover:italic' href='#' onclick='loadMore'>Load More</a>";
+doPosts.appendChild(btn);
+
+function loadMore() {}
